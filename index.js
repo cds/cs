b34f79f025b2ad3be8ae4b92eca3eb7871fefe61
chandrakantsangale <c.sangale@gmail.com>
2020-08-01 19:05:11 +0530

    added redme

---
+++ index.js
@@ -0,0 +1,18 @@
+function log(msg) {
+  console.log(msg);
+}
+function shout(msg) {
+  if (typeof msg === str) {
+    console.log(msg.toUpperCase());
+  } else {
+    log(msg);
+  }
+}
+function whisper(msg) {
+  if (typeof msg === str) {
+    console.log(msg.toUpperCase());
+  } else {
+    log(msg);
+  }
+}
+module.exports = { log, shout, whisper };
